1ed1c1ec323bd89467e9e766ae495eff3ef587c9
plasma-gith <md.iftekharzawad@gmail.com>
2024-07-16 19:41:02 +0600

    typo

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'RedX',
+        title: 'Redex',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
             appBarTheme: const AppBarTheme(
